a3dab7f760a0452d27efbcac7bc0df979002032e
w-liuyingjie <1647460283@qq.com>
2025-10-21 16:27:46 +0800

    添加喜爱页面，添加菜单页面

--- lib/page/favorities.dart
+++ lib/page/favorities.dart
@@ -1,4 +1,7 @@
+import 'package:first_flutter_project/page/common_title.dart';
+import 'package:first_flutter_project/page/search/food_grid_view2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // 喜爱
 class Favorities extends StatefulWidget {
@@ -11,6 +14,16 @@ class Favorities extends StatefulWidget {
 class _FavoritiesState extends State<Favorities> {
   @override
   Widget build(BuildContext context) {
-    return Text('123123');
+    return Container(
+      padding: EdgeInsetsGeometry.only(left: 20.w, right: 20.w),
+      child: Column(
+        children: [
+          SizedBox(height: 20.h),
+          CommonTitle(title: 'Favorites'),
+          SizedBox(height: 20.h),
+          Expanded(child: FoodGridView2()),
+        ],
+      ),
+    );
   }
 }
--- lib/page/recipe.dart
+++ lib/page/recipe.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'common_title.dart';
 
 class Ricipe extends StatefulWidget {
   const Ricipe({super.key});
@@ -8,8 +11,51 @@ class Ricipe extends StatefulWidget {
 }
 
 class _RicipeState extends State<Ricipe> {
+  List<String> label = [
+    'NAME',
+    'CATEGORY',
+    'DESCRIPTION',
+    'INGREDIENTS',
+    'DESCRIPTION',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Text('Ricipe');
+    return Container(
+      padding: EdgeInsetsGeometry.only(left: 20.w, right: 20.w),
+      child: Column(
+        children: [
+          SizedBox(height: 20.h),
+          CommonTitle(title: 'Recipe'),
+          SizedBox(height: 20.h),
+          Expanded(
+            child: ListView(
+              children: List.generate(label.length, (index) {
+                return buildTextFormItem(label[index]);
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTextFormItem(String title) {
+    return SizedBox(
+      height: 75.h,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(title),
+          SizedBox(height: 5.h),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              fillColor: Color(0xFFF1F1F1),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
