43239badd12384c47ca3445ef9572b71e0e197d9
w-liuyingjie <1647460283@qq.com>
2025-10-27 10:18:01 +0800

    添加生成的环境配置.g文件

--- lib/base/env.dart
+++ lib/base/env.dart
@@ -2,8 +2,11 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
+//obfuscate true开启为是否混淆
 @Envied(obfuscate: false, requireEnvFile: true, path: ".env")
 abstract class Env {
-  @EnviedField(varName: "baseUrl")
+  @EnviedField(varName: "BASE_URL")
   static final String baseUrl = _Env.baseUrl;
+
+
 }
